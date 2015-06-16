36c6c9da39fd7ea2b51aeb25f1370b4b220d2529
Antonella P <antonella.perna@yahoo.com>
2015-06-16 19:57:05 +0200

    Superminimal Theme Footer

---
+++ superminimal/footer.php
@@ -0,0 +1,8 @@
+        <footer id="footer">
+            
+            <?php echo get_theme_mod( 'superminimal_footer_text', 'Superminimal Theme &ndash; Kirki Toolkit Demo for SitePoint' ); ?>
+			
+			<?php wp_footer(); ?>
+        </footer><!-- /#footer -->
+    </body>
+</html>
