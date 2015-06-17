3813ce3a088f17e0e3cd4211fcd3f9e6d4254f41
Antonella P <antonella.perna@yahoo.com>
2015-06-17 11:41:56 +0200

    Superminimal demo theme footer.php file

--- superminimal/footer.php
+++ superminimal/footer.php
@@ -1,6 +1,6 @@
         <footer id="footer">
             
-            <?php echo get_theme_mod( 'superminimal_footer_text', 'Superminimal Theme &ndash; Kirki Toolkit Demo for SitePoint' ); ?>
+            <?php echo get_theme_mod( 'superminimal_footer_text', __( 'Superminimal Theme &ndash; Kirki Toolkit Demo for SitePoint' ) ); ?>
 			
 			<?php wp_footer(); ?>
         </footer><!-- /#footer -->
